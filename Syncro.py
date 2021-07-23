058a1fe13cc5e53883586d2f5aa6e6b906983682
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2021-07-22 22:11:41 -0300

    major refactor

--- Syncro.py
+++ Syncro.py
@@ -1,133 +1,112 @@
+from typing import Dict, List
+
+
 class SyncroGame:
-    def __init__(self, level, max_iter, state, history=None, _state=None):
+    translation = {
+        "square": "□",
+        "circle": "O",
+        "triangle": "△",
+    }
+
+    def __init__(
+        self, level: int, cardinality: int, max_iter: int, rules: Dict, debug=False
+    ):
         self.level = level
         self.max_iter = max_iter
-        self.history = history if history else []
-        self.state = _state if _state else state
+        self.cardinality = cardinality
+        self.rules = rules
+        self.state = [1] * cardinality
+        self.history = []
+        self._seen = set()
+        self.debug = debug
+
+    def set_state(self, state: List[int]):
+        self.state = state
+        return self
+
+    def set_history(self, history: List[str]):
+        self.history = history
+        return self
+
+    def get_history(self) -> List[str]:
+        return [self.translation[x] for x in self.history]
+
+    def __repr__(self) -> str:
+        return f"(L{self.level}) state={self.state}, history={self.get_history()}"
 
     def __str__(self) -> str:
-        return f"state={self.state}, history={str(self.history)}"
+        return f"Level {self.level}: {self.get_history()}"
 
-    def clone(self):
-        new = type(self)(_state=self.state, history=[x for x in self.history])
-        return new
-
-    def check(self):
-        return max(self.state) == len(self.state)
-
-    def __call__(self, move: str):
-        if move == "square":
-            return self.square()
-        elif move == "circle":
-            return self.circle()
-        elif move == "triangle":
-            return self.triangle()
-        else:
-            raise RuntimeError(f"This move ({move})) is not supported")
+    def check(self, state=None) -> bool:
+        if state is None:
+            state = self.state
+        if sum(state) != len(state):
+            print(
+                f"Something is wrong. Maybe the rules are not well encoded. {self} (real state: {state})"
+            )
+        return max(state) == len(state)
 
     def solve(self):
-        solution = self._solve()
-        if solution:
-            print(f"Level {solution.level}: {solution.history}")
-        else:
-            print(f"Level {self.level}: FAILED")
+        print(self._solve(self.state, []))
 
-    def _solve(self):
-        if self.check():
+    def _solve(self, state: List, history: List):
+        if self.debug:
+            print(f"debug: {state}, {history}")
+
+        if self.check(state):
+            self.set_state(state).set_history(history)
             return self
-        if len(self.history) >= self.max_iter:
+        if len(history) >= self.max_iter:
+            return None
+        if str(state) in self._seen:
             return None
 
+        self._seen.add(str(state))
         for move in ["square", "circle", "triangle"]:
-            next_state = self.clone()
-            result = next_state(move)._solve()
+            try:
+                result = self._solve(self.rules[move](state), [*history, move])
+            except KeyError:
+                pass
             if result:
                 return result
-
-    def square(self):
-        if len(self.history) < self.max_iter:
-            self.history.append("□")
-        return self
-
-    def circle(self):
-        if len(self.history) < self.max_iter:
-            self.history.append("O")
-        return self
-
-    def triangle(self):
-        if len(self.history) < self.max_iter:
-            self.history.append("△")
-        return self
 
 
 class Level_0(SyncroGame):
     def __init__(self, *args, **kwargs):
-        super().__init__(level=0, max_iter=1, state=[1, 1], *args, **kwargs)
-
-    def square(self):
-        self.state = [0, 2]
-        return super().square()
+        rules = {"square": lambda state: [0, 2]}
+        super().__init__(
+            level=0, max_iter=1, cardinality=2, rules=rules, *args, **kwargs
+        )
 
 
 class Level_1(SyncroGame):
     def __init__(self, *args, **kwargs):
-        super().__init__(level=1, max_iter=1, state=[1, 1], *args, **kwargs)
-
-    def square(self):
-        self.state = [0, 2]
-        return super().square()
-
-    def circle(self):
-        self.state = [1, 1]
-        return super().circle()
+        rules = {
+            "square": lambda state: [0, 2],
+            "circle": lambda state: [1, 1],
+        }
+        super().__init__(
+            level=1, max_iter=1, cardinality=2, rules=rules, *args, **kwargs
+        )
 
 
 class Level_2(SyncroGame):
     def __init__(self, *args, **kwargs):
-        super().__init__(level=2, max_iter=2, state=[1, 1, 1], *args, **kwargs)
-
-    def square(self):
-        prev = [x for x in self.state]
-        self.state = [prev[2], *prev[:2]]
-        return super().square()
-
-    def circle(self):
-        prev = [x for x in self.state]
-        self.state[0] = prev[1]
-        self.state[1] = 0
-        self.state[2] = prev[2] + prev[0]
-        return super().circle()
+        rules = {
+            "square": lambda state: [state[2], *state[:2]],
+            "circle": lambda state: [state[1], 0, state[0] + state[2]],
+        }
+        super().__init__(
+            level=2, max_iter=2, cardinality=3, rules=rules, *args, **kwargs
+        )
 
 
 class Level_3(SyncroGame):
     def __init__(self, *args, **kwargs):
-        super().__init__(level=3, max_iter=3, state=[1, 1, 1], *args, **kwargs)
-
-    def square(self):
-        prev = [x for x in self.state]
-        self.state[0] = prev[2]
-        self.state[1] = 0
-        self.state[2] = prev[0] + prev[1]
-        return super().square()
-
-    def circle(self):
-        prev = [x for x in self.state]
-        self.state = [prev[2], *prev[:2]]
-        return super().circle()
-
-
-# template
-class Level_Z(SyncroGame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(level=0, max_iter=0,
-                         state=[1, 1, 1, 1], *args, **kwargs)
-
-    def square(self):
-        prev = [x for x in self.state]
-
-        return super().square()
-
-    def circle(self):
-        prev = [x for x in self.state]
-
-        return super().circle()
+        rules = {
+            "square": lambda state: [state[2], 0, state[0] + state[1]],
+            "circle": lambda state: [state[2], *state[:2]],
+        }
+        super().__init__(
+            level=3, max_iter=3, cardinality=3, rules=rules, *args, **kwargs
+        )
--- syncrosolver.py
+++ syncrosolver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3¡
+#!/usr/bin/env python3
 import Syncro
 
 
